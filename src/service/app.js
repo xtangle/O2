18a9304319235cc83e6a6263469369741cb8836c
xtangle <jackyx5724@hotmail.com>
2017-01-03 16:36:50 -0500

    JX - Added planetaryjs globe to index page

--- src/service/app.js
+++ src/service/app.js
@@ -2,6 +2,9 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+// first parameter is the mount point, second is the location in the file system
+app.use("/public", express.static(path.resolve(__dirname, '../../public')));
+
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
   res.sendFile(path.resolve(__dirname, '../../index.html'));
