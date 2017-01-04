67d6d4cceee58165e1c1e71fc85e8d3273beb451
xtangle <jackyx5724@hotmail.com>
2017-01-04 03:27:34 -0500

    JX - Moved all web app files into the webapp folder; updated README with installation instructions

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # RBC-Hackathon
 Repository for team O2's entry in the RBC Hackathon
+
+## Installation
+1. Clone the project to a local directory.
+2. Navigate to the project root directory and run `npm install` on the command line.
+
+## Starting the web server
+1. At the project root directory, run `node src/webapp/server.js`.
+2. Open up a web browser and navigate to `localhost:8080`.
--- src/webapp/server.js
+++ src/webapp/server.js
@@ -3,11 +3,11 @@ var path = require('path');
 var app = express();
 
 // first parameter is the mount point, second is the location in the file system
-app.use("/public", express.static(path.resolve(__dirname, '../../public')));
+app.use("/static", express.static(path.resolve(__dirname, 'static')));
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
-  res.sendFile(path.resolve(__dirname, '../../index.html'));
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
 app.listen(8080);
