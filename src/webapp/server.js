1106dd7fe99143cf40c5616ec8458d3c3db745aa
xtangle <jackyx5724@hotmail.com>
2017-01-04 12:10:50 -0500

    JX - Change web server to always serve content over SSL

--- src/webapp/server.js
+++ src/webapp/server.js
@@ -5,8 +5,22 @@ var cfenv = require('cfenv');
 
 var app = express();
 
+app.enable('trust proxy');
+
 // serve the files out of ./public as our main files
 app.use(express.static(path.resolve(__dirname, 'public')));
+
+// Add a handler to inspect the req.secure flag (see http://expressjs.com/api#req.secure).
+// This allows us to know whether the request was via http or https.
+app.use (function (req, res, next) {
+  if (req.secure) {
+    // request was via https, so do no special handling
+    next();
+  } else {
+    // request was via http, so redirect to https
+    res.redirect('https://' + req.headers.host + req.url);
+  }
+});
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
