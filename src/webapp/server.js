d1005a9b8c3144dfb38b5644e1bf4dcab97f0d33
xtangle <jackyx5724@hotmail.com>
2017-01-04 11:00:56 -0500

    JX - Moved index.html inside public folder

--- src/webapp/server.js
+++ src/webapp/server.js
@@ -3,11 +3,11 @@ var path = require('path');
 var app = express();
 
 // first parameter is the mount point, second is the location in the file system
-app.use("/static", express.static(path.resolve(__dirname, 'static')));
+app.use("/public", express.static(path.resolve(__dirname, 'public')));
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
-  res.sendFile(path.resolve(__dirname, 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'public/index.html'));
 });
 
 app.listen(8080);
