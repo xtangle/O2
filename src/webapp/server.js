1af5ee423fddc471511919480b85c8203865446a
xtangle <jackyx5724@hotmail.com>
2017-01-04 12:36:22 -0500

    JX - Clean up comments

--- src/webapp/server.js
+++ src/webapp/server.js
@@ -5,20 +5,6 @@ var cfenv = require('cfenv');
 
 var app = express();
 
-//app.enable('trust proxy');
-
-// Add a handler to inspect the req.secure flag (see http://expressjs.com/api#req.secure).
-// This allows us to know whether the request was via http or https.
-/*app.use (function (req, res, next) {
-  if (req.secure) {
-    // request was via https, so do no special handling
-    next();
-  } else {
-    // request was via http, so redirect to https
-    res.redirect('https://' + req.headers.host + req.url);
-  }
-});*/
-
 // serve the files out of ./public as our main files
 app.use(express.static(path.resolve(__dirname, 'public')));
 
