cbf6c1218c14a8f0bfbdd83d896914b150449a73
xtangle <jackyx5724@hotmail.com>
2017-01-05 01:50:53 -0500

    JX - refactor project structure, distinguish web server from rest server

--- src/web-server.js
+++ src/web-server.js
@@ -14,5 +14,5 @@ var appEnv = cfenv.getAppEnv();
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+  console.log("web server starting on " + appEnv.url);
 });
