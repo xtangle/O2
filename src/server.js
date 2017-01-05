9be8701f4fe3dc3bbd78ca145a79099901a1128f
xtangle <jackyx5724@hotmail.com>
2017-01-05 04:31:41 -0500

    JX - added ability to retrieve all transactions from mongodb using mongoose

--- src/server.js
+++ src/server.js
@@ -2,22 +2,32 @@ var express = require('express');
 var path = require('path');
 // cfenv provides access to your Cloud Foundry environment, for info see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
+var connectionManager = require('./connection-manager');
 
 var app = express();
-var router = express.Router();
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
+
+// initialize the database connection
+connectionManager.init();
 
 // serve our static content out of ./public
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 // serve our RESTful services
-router.get('/', function(req, res) {
-  res.json({ message: 'hooray! welcome to our api!' });
+var router = express.Router();
+router.get('/transactions', function(req, res) {
+  var Transaction = require('./transaction/transaction-model');
+  Transaction.find({}, function(err, transactions) {
+    if (err) throw err;
+    res.json({
+      message: 'hooray! welcome to our api!',
+      transactions: transactions
+    });
+  });
 });
 app.use('/api', router);
 
 // start server on the specified port and binding host
+var appEnv = cfenv.getAppEnv();
 app.listen(appEnv.port, appEnv.bind, function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
