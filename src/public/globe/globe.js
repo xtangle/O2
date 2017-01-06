7f89d3b7fbf5320f2e0a2cdcb558991329763892
xtangle <jackyx5724@hotmail.com>
2017-01-06 06:03:32 -0500

    JX - adjust globe scrolling sensitivity when zoomed in/out

--- src/public/globe/globe.js
+++ src/public/globe/globe.js
@@ -94,10 +94,11 @@ function ready(error, world, countryData) {
       }))
     .call(d3.behavior.zoom()
       .scale(projection.scale())
-      .scaleExtent([200, 800])
+      .scaleExtent([200, 1000])
       .on('zoom', function () {
         var scale = d3.event.scale;
         projection.scale(scale);
+        sens = 0.25 - 0.2*((scale - 245)/(1000 - 245));
         svg.selectAll('path.land').attr('d', path);
         svg.selectAll('path.water').attr('d', path);
         svg.selectAll('path.lake').attr('d', path);
