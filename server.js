89d38fd0a859428552d9e99b0d4767c7fe7555d5
xtangle <jackyx5724@hotmail.com>
2017-01-06 12:42:32 -0500

    merge and fix server.js problems

--- server.js
+++ server.js
@@ -2,17 +2,35 @@ var express = require('express');
 var path = require('path');
 // cfenv provides access to your Cloud Foundry environment, for info see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
+var connectionManager = require('./src/connection-manager');
 
 var app = express();
+
+// initialize the database connection
+connectionManager.init();
+
+// serve the files out of ./public as our main files
+app.use(express.static(path.resolve(__dirname, 'src/public')));
+
+// serve our RESTful services
+var router = express.Router();
+router.get('/transactions', function(req, res) {
+  var Transaction = require('./src/transaction/transaction-model');
+  Transaction.find({}, function(err, transactions) {
+    if (err) throw err;
+    res.json({
+      message: 'hooray! welcome to our api!',
+      transactions: transactions
+    });
+  });
+});
+app.use('/api', router);
+
+// Socket.io
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var port = 8080;
- 
-// serve the files out of ./public as our main files
-app.use(express.static(path.resolve(__dirname, 'public')));
-
-var buyT = function(){
-  return Math.floor((Math.random() * 100) + 1);;
+var buyT = function() {
+  return Math.floor((Math.random() * 100) + 1);
 };
 io.on('connection', function(socket) {
   console.log('new connection made');
@@ -24,16 +42,11 @@ io.on('connection', function(socket) {
       }
     });
   }, 1000);
-
 });
 
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
-server.listen(port, function() {
-  console.log("Listening on port " + port);
-});
 // start server on the specified port and binding host
-// app.listen(appEnv.port, '0.0.0.0', function() {
-//   // print a message when the server starts listening
-//   console.log("server starting on " + appEnv.url);
-// });
+var appEnv = cfenv.getAppEnv();
+server.listen(appEnv.port, appEnv.bind, function() {
+  // print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
+});
