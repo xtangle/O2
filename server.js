7a79c09d743b4cf5f1bcc01ffc1a453dc8520f09
xtangle <jackyx5724@hotmail.com>
2017-01-08 13:22:42 -0500

    JX - generate better sample data

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.use(express.static(path.resolve(__dirname, 'src/public')));
 
 // serve our RESTful services
 var router = express.Router();
-router.get('/transactions', function(req, res) {
+router.get('/transactions', function (req, res) {
   var Transaction = require('./src/transaction/transaction-model');
-  Transaction.find({}, function(err, transactions) {
+  Transaction.find({}, function (err, transactions) {
     if (err) throw err;
     res.json({
       message: 'hooray! welcome to our api!',
@@ -31,23 +31,28 @@ app.use('/api', router);
 // Socket.io
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-io.on('connection', function(socket) {
+io.on('connection', function (socket) {
   console.log('new connection made');
 
   var currencies = ['CAD', 'USD', 'EUR', 'GBP', 'CNY', 'JPY', 'INR', 'AUD'];
-  var minAmount = 1000, maxAmount = 100000;
 
   function getRandomCurrency() {
     return currencies[Math.floor(Math.random() * currencies.length)];
   }
 
   function getRandomAmount() {
-    var amount = Math.floor(Math.random() * (maxAmount - minAmount)) + minAmount;
-    var sign = Math.random() < 0.5 ? -1 : 1;
+    function randNorm() {
+      var u = 1 - Math.random();
+      var v = 1 - Math.random();
+      return Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
+    }
+    var sigma = 100000, mu = 5000;
+    var amount = Math.pow(randNorm(), 2) * sigma + mu;
+    var sign = Math.random() < 0.49 ? -1 : 1;
     return sign * amount;
   }
 
-  var autoEmit = setInterval(function() {
+  var autoEmit = setInterval(function () {
     socket.emit('message-from-server', {
       transaction: {
         settlementCurrency: getRandomCurrency(),
@@ -59,7 +64,7 @@ io.on('connection', function(socket) {
 });
 
 // start server on the specified port and binding host
-server.listen(appEnv.port, appEnv.bind, function() {
+server.listen(appEnv.port, appEnv.bind, function () {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
